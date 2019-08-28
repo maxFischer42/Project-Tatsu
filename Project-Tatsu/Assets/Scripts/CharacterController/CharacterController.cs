3f35449bca2d73e90e0e72321ee314a4996d00f0
Max <maxfischer072@gmail.com>
2019-08-27 21:29:35 -0700

    Created Character Controller and Character Data scripts

---
+++ Project-Tatsu/Assets/Scripts/CharacterController/CharacterController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CharacterController
+{
+    public class CharacterController : MonoBehaviour
+    {
+        public CharacterData data;
+        void Start()
+        {
+            print(data.characterName + ", " + data.movement);
+        }
+    }
+}
---
+++ Project-Tatsu/Assets/Scripts/CharacterController/CharacterData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Characters/CharacterData")]
+public class CharacterData : ScriptableObject
+{
+    public string characterName = "null";
+    public float movement = 0f;
+}
