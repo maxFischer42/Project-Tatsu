56f7bcea48cd1fa3ae3de71c07dd07e91016776b
Max <maxfischer072@gmail.com>
2019-08-28 22:56:39 -0700

    Implemented AI Events

---
+++ Project-Tatsu/Assets/Scripts/AI/Events/EventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHandler : MonoBehaviour
+{
+    public AiEvent[] events;
+    private int currentEvent = 0;
+
+    void Update()
+    {
+
+    }
+}
---
+++ Project-Tatsu/Assets/Scripts/Input/AIInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Controller;
+using Inputs;
+
+public class AIInputs : MonoBehaviour
+{
+    public Controller.CharacterController characterController;
+    public Vector2 movement;
+
+    private void Update()
+    {
+        InputController currentFrameInput = new InputController();
+        currentFrameInput = SetDirectionInputs(currentFrameInput);
+
+        characterController.SetInputs(currentFrameInput);
+    }
+
+    InputController SetDirectionInputs(InputController input)
+    {
+        input.direction = movement;
+        return input;
+    }
+}
